2e460a6f5933ac88e568345e2514580a1af3ceb5
dhornback10 <you@example.com>
2018-03-06 11:27:22 -0500

    added events json and routes & server

---
+++ js/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var events = require("./routes.js");
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use("/", events);
+
+
+var server = app.listen(3005, function() {
+    var port = server.address().port;
+    console.log("Express server is up and running", port);
+});
