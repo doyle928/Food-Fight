4df01dc8d8116e3fe1b834cf38a12189a80c6b7e
dhornback10 <you@example.com>
2018-03-07 17:10:53 -0500

    Made some progress on connecting to the database, but we don't quite have it.

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var util = require("util");
+var routes = require("./routes");
 //var events = require("./routes.js");
 var app = express();
+app.use("/", routes);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 //app.use("/", events);
 
 var port = process.env.PORT || 5000;
-var server = app.listen(port, function() {
+var server = app.listen(5000, function() {
   var port = server.address().port;
   console.log("Express server is up and running @ ", port);
 });
