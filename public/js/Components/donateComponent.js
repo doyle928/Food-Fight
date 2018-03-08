c835d3c26729630d8c6c9b632a9c03fceefc668a
Todd Rader <todd@toddrader.com>
2018-03-08 15:22:27 -0500

    results and donate pages

---
+++ public/js/Components/donateComponent.js
@@ -0,0 +1,7 @@
+(function() {
+  var donateComponent = {
+    templateUrl: "Partials/donate.html",
+    controller: "controllerFunction"
+  };
+  angular.module("App").component("donateComponent", resultsComponent);
+})();
