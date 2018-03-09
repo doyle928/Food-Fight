94da9162463425b1f7463124049acd90717be9a5
dhornback10 <you@example.com>
2018-03-09 11:57:34 -0500

    Donations page is done

--- public/js/Components/donateComponent.js
+++ public/js/Components/donateComponent.js
@@ -1,7 +1,12 @@
 (function() {
-  var donateComponent = {
-    templateUrl: "Partials/donate.html",
-    controller: "controllerFunction"
-  };
-  angular.module("App").component("donateComponent", donateComponent);
+    var donateComponent = {
+        templateUrl: "Partials/donate.html",
+        controller: function($location) {
+            const $ctrl = this;
+            $ctrl.home = function() {
+                $location.path("/welcome");
+            };
+        }
+    }
+    angular.module("App").component("donateComponent", donateComponent);
 })();
